0f07b05c85191d01d7e28f2f22512baaedd123d0
tomasina <tatiana.anofrenko@gmail.com>
2015-01-27 08:41:27 -0800

    first task

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'cards/index'
+  get 'cards/show'
+  get 'cards/edit'
+  get 'cards/destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root :to=>'cards#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
