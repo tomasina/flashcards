83f0bc2c4c2d0a5b8771bfdfca6daf94608e179f
tomasina <tatiana.anofrenko@gmail.com>
2015-02-06 07:11:43 -0800

    changes done per comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'cards/index'
-  get 'cards/show'
-  get 'cards/edit'
-  get 'cards/destroy'
-
+    get "home" =>'home#index'
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root :to=>'cards#index'
+  # root :to=>'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -58,4 +55,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
