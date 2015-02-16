ca6d672e4aba6fd9214383492232dd6d2c1369ff
tomasina <tatiana.anofrenko@gmail.com>
2015-02-16 08:43:52 -0800

    second task

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,13 @@
 class Card < ActiveRecord::Base
-  def new
-  end
-
   def create
+  @card = Card.new(card_params)
+
+  @card.save
+  redirect_to @card
+end
+
+private
+  def card_params
+    params.require(:card).permit(:original_text, :translated_text, :review_date)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get "home" => 'home#index'
 end
+
+Flashcards::Application.routes.draw do
+  resources :cards
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
