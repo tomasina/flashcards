f3e53b1353c0d1f65ba35e22b2820c75f0d44eba
tomasina <tatiana.anofrenko@gmail.com>
2015-02-16 13:29:23 -0800

    hope last comments

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,9 +1,9 @@
 class Card < ActiveRecord::Base
   def create
     @card = Card.new(params[:card])
-end
+  end
 
-private
+  private
   def card_params
     params.require(:card).permit(:original_text, :translated_text, :review_date)
   end
