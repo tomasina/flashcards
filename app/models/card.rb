4bc1b72586fb88d0380098a9c6df55e7e3e5d84e
tomasina <tatiana.anofrenko@gmail.com>
2015-02-25 08:37:42 -0800

    validation added

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,9 +1,17 @@
 class Card < ActiveRecord::Base
-  validates :original_text, :translated_text, presence: true
 
-  #def create
-   # Card.create(card_params)
- # end
+  validates :original_text, :translated_text, :review_date, presence: true
+
+  validate :valid_original
+  def valid_original
+    if original_text.to_s.downcase == translated_text.to_s.downcase
+      errors.add(:original_text, "texts shouldn't match")
+    end
+  end
+
+  def create
+    Card.create(card_params)
+  end
 
   private
     def card_params
